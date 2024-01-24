671472e6999b6824905f2ede55ca31f895e698a0
HyunseoY <maricat0715@gmail.com>
2024-01-24 17:19:24 +0900

    위치 및 크기 수정

--- src/components/Main/Main.styles.ts
+++ src/components/Main/Main.styles.ts
@@ -6,76 +6,33 @@ export const Container = styled.div`
   align-items: center;
   flex-direction: column;
   margin: auto;
-  text-align: center;
+  margin-bottom: 10vh;
   padding: 50px 0;
-  color: white;
-  text-shadow: 1px 1px 1px #bc8989;
   background: linear-gradient(${colors.pink} 50%, transparent 50%);
 `;
 
-export const Title = styled.h1`
-  position: absolute;
-  top: 6vh;
-  font-size: 30px;
-
-  @media (max-width: 768px) {
-    top: 7vh;
-    font-size: 25px;
-  }
-`;
-
-export const NameImage = styled.img`
-  width: 90%;
-  height: 100px;
-  padding: 20px;
-  object-fit: contain;
-  z-index: 2;
-`;
-
 export const MainImage = styled.img`
-  position: relative;
-  width: 80%;
-  top: 40px;
-  margin-top: -15vh;
-  z-index: 1;
+  width: 90%;
 `;
-
-// export const Datebox = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   align-items: center;
-//   color: ${colors.content1};
-//   text-shadow: none;
-// `;
 
 export const DateImage = styled.img`
   position: relative;
-  width: 80%;
-  top: 10vh;
+  width: 77%;
+  margin-top: 5%;
 `;
 
 // export const TextBox = styled.div`
+//   position: relative;
 //   display: flex;
 //   flex-direction: column;
 //   justify-content: center;
 //   align-items: center;
-//   padding: 0 0 0 70px;
+//   width: 77%;
+//   bottom: 10vh;
+//   z-index: 1;
 // `;
 
-// export const NameText = styled.span`
-//   position: absolute;
-//   width: 60%;
-//   bottom: 235px;
-// `;
-// export const PlaceText = styled.span`
-//   position: absolute;
-//   width: 60%;
-//   bottom: 215px;
-// `;
-
-// export const DateText = styled.span`
-//   position: absolute;
-//   width: 60%;
-//   bottom: 194px;
+// export const Text = styled.span`
+//   padding: 0.5vh 0 0 10vh;
+//   color: ${colors.content1};
 // `;
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,14 +1,11 @@
 import date from '../../assets/date.png';
-import name from '../../assets/name.png';
-import wedding from '../../assets/wedding.png';
+import wedding2 from '../../assets/wedding2.png';
 import * as Styled from './Main.styles';
 
 export const Main = () => {
   return (
     <Styled.Container>
-      <Styled.Title>THE MARRIAGE OF</Styled.Title>
-      <Styled.NameImage src={name} alt="name" />
-      <Styled.MainImage src={wedding} alt="wedding" />
+      <Styled.MainImage src={wedding2} alt="wedding" />
       <Styled.DateImage src={date} alt="date" />
     </Styled.Container>
   );
