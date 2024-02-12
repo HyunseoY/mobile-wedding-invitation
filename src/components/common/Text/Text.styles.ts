56b458c270934a967c2e3fa5747cd9d84589ee07
HyunseoY <maricat0715@gmail.com>
2024-02-13 03:49:39 +0900

    댓글 줄간격 수정

--- src/components/common/Text/Text.styles.ts
+++ src/components/common/Text/Text.styles.ts
@@ -1,10 +1,12 @@
-import styled from '@emotion/styled';
-import { colors } from '../../../styles/theme/colors';
-import { TextProps } from './Text.types';
+import styled from "@emotion/styled";
+import { colors } from "../../../styles/theme/colors";
+import { TextProps } from "./Text.types";
 
 export const Text = styled.p<TextProps>`
-  font-size: ${(props) => props.fontSize || '1rem'};
-  color: ${(props) => (props.color ? colors[props.color] : '#585858')};
-  letter-spacing: ${(props) => props.letterSpacing || 'normal'};
-  margin-left: ${(props) => props.margin || 'none'};
+  font-size: ${(props) => props.fontSize || "1rem"};
+  color: ${(props) => (props.color ? colors[props.color] : "#585858")};
+  letter-spacing: ${(props) => props.letterSpacing || "normal"};
+  margin-left: ${(props) => props.margin || "none"};
+
+  line-height: 1.2em;
 `;
