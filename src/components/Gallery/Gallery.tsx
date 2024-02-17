adac048203e70ea19e564b9dac5d78c7c4bb57f5
HyunseoY <maricat0715@gmail.com>
2024-02-18 02:57:45 +0900

    이미지 로딩 수정

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Fade } from "react-awesome-reveal";
 import shoot1 from "../../assets/shoot1.png";
 import shoot10 from "../../assets/shoot10.png";
@@ -14,6 +15,27 @@ import * as Styled from "./Gallery.styles";
 
 export const Gallery = () => {
   const images = [shoot3, shoot2, shoot1, shoot4, shoot5, shoot6, shoot7, shoot8, shoot10, shoot11];
+
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadImage = (src: string) => {
+      return new Promise((resolve, reject) => {
+        const image = new Image();
+        image.src = src;
+        image.onload = resolve;
+        image.onerror = reject;
+      });
+    };
+
+    Promise.all(images.map((src) => loadImage(src)))
+      .then(() => setLoaded(true))
+      .catch((error) => console.log("Failed to load images", error));
+  }, [images]);
+
+  if (!loaded) {
+    return null; // 또는 로딩 스피너를 표시
+  }
 
   return (
     <Styled.Container>
