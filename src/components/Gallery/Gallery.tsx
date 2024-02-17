5b71605b77dae847a82b3594d6705b5671ec0d24
HyunseoY <maricat0715@gmail.com>
2024-02-17 20:14:23 +0900

    갤러리 사진 수정

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -12,7 +12,7 @@ import { Carousel, Title } from "../common";
 import * as Styled from "./Gallery.styles";
 
 export const Gallery = () => {
-  const images = [shoot1, shoot2, shoot3, shoot4, shoot5, shoot6, shoot7, shoot8, shoot10, shoot11];
+  const images = [shoot3, shoot2, shoot1, shoot4, shoot5, shoot6, shoot7, shoot8, shoot10, shoot11];
 
   return (
     <Styled.Container>
