aeb6236d94f31c0a6fb0ff862e5d8f8f19c4e67f
HyunseoY <maricat0715@gmail.com>
2024-02-17 19:28:29 +0900

    description 변경

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -8,24 +8,11 @@ import shoot5 from "../../assets/shoot5.png";
 import shoot6 from "../../assets/shoot6.png";
 import shoot7 from "../../assets/shoot7.png";
 import shoot8 from "../../assets/shoot8.png";
-import shoot9 from "../../assets/shoot9.png";
 import { Carousel, Title } from "../common";
 import * as Styled from "./Gallery.styles";
 
 export const Gallery = () => {
-  const images = [
-    shoot1,
-    shoot2,
-    shoot3,
-    shoot4,
-    shoot5,
-    shoot6,
-    shoot7,
-    shoot8,
-    shoot9,
-    shoot10,
-    shoot11
-  ];
+  const images = [shoot1, shoot2, shoot3, shoot4, shoot5, shoot6, shoot7, shoot8, shoot10, shoot11];
 
   return (
     <Styled.Container>
